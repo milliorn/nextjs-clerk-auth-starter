7d1897a27e411f612f1a7ecc1ee7c09488bbbb22
milliorn <scottmilliorn@gmai.com>
2023-09-13 22:05:01 -0700

    Profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,10 @@
+// Import the UserProfile component from Clerk/Next.js
 import { UserProfile } from "@clerk/nextjs";
 
+// Define the type for the component's props
 type Props = {};
 
+// Create the Profile component
 const Profile = (props: Props) => {
   return (
     <div>
