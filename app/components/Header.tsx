837bfc3b2c1223037195a6512a1a49ba309ce20d
milliorn <scottmilliorn@gmai.com>
2023-09-13 22:02:48 -0700

    Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,16 +1,21 @@
-import { auth, UserButton } from "@clerk/nextjs";
-import Link from "next/link";
+import { auth, UserButton } from "@clerk/nextjs"; // Clerk authentication
+import Link from "next/link"; // Next.js link component for navigation
 
+// Define the type for the component's props
 type Props = {};
 
+// Create the Header component
 const Header = (props: Props) => {
+  // Use Clerk's auth() function to get the user's ID (if authenticated)
   const { userId } = auth();
-  // console.log(userId);
 
+  // Render the header section
   return (
     <div>
-      <nav className="bg-indigo-900  text-indigo-50 p-4 flex items-center justify-between mb-4 dark:bg-indigo-600">
+      {/* Navigation bar with styling */}
+      <nav className="bg-indigo-900 text-indigo-50 p-4 flex items-center justify-between mb-4 dark:bg-indigo-600">
         <div className="flex items-center">
+          {/* Create a link to the home page */}
           <Link href="/">
             <div className="hover:text-indigo-300 text-sm uppercase font-bold">
               Clerk Starter
@@ -18,8 +23,10 @@ const Header = (props: Props) => {
           </Link>
         </div>
         <div className="flex items-center">
+          {/* Conditional rendering based on user authentication */}
           {!userId && (
             <>
+              {/* Display login and registration links if user is not authenticated */}
               <Link href="login" className="hover:text-indigo-300 mr-4">
                 LogIn
               </Link>
@@ -29,11 +36,13 @@ const Header = (props: Props) => {
             </>
           )}
           {userId && (
+            // Display the profile link if the user is authenticated
             <Link href="profile" className="hover:text-indigo-300 mr-4">
               Profile
             </Link>
           )}
           <div className="ml-auto">
+            {/* Display a user button for authentication, redirects to the home page after sign-out */}
             <UserButton afterSignOutUrl="/" />
           </div>
         </div>
@@ -42,4 +51,5 @@ const Header = (props: Props) => {
   );
 };
 
+// Export the Header component as the default export
 export default Header;
