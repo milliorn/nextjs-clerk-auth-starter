c6a911a5f518a42f76339bdaeae2ebabec133a9a
milliorn <scottmilliorn@gmai.com>
2023-09-13 22:07:10 -0700

    RootLayout, registration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+// Import required dependencies and components
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import type { Metadata } from "next";
@@ -5,28 +6,36 @@ import { Inter } from "next/font/google";
 import Header from "./components/Header";
 import "./globals.css";
 
+// Create a custom font using the Inter font family with Latin subset
 const inter = Inter({ subsets: ["latin"] });
 
+// Define metadata for the application
 export const metadata: Metadata = {
   title: "Next.js + Clerk Starter",
   description: "A starter for Next.js + Clerk",
 };
 
+// Define the RootLayout component, which wraps the entire application
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
+    // Wrap the entire application with ClerkProvider to enable Clerk functionality
     <ClerkProvider
       appearance={{
-        baseTheme: dark,
+        baseTheme: dark, // Use the dark theme from Clerk
       }}
     >
       <html lang="en">
         <body className={inter.className}>
+          {/* Main content container */}
           <main className="2xl:container mx-auto dark:bg-indigo-900 dark:text-indigo-50 text-indigo-50">
+            {/* Include the Header component */}
             <Header />
+
+            {/* Content area */}
             <div className="flex items-start justify-center min-h-screen">
               <div className="mt-20">{children}</div>
             </div>
--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -1,7 +1,10 @@
+// Import the SignUp component from Clerk/Next.js
 import { SignUp } from "@clerk/nextjs";
 
+// Define the type for the component's props
 type Props = {};
 
+// Create the SignUpPage component
 const SignUpPage = (props: Props) => {
   return <SignUp />;
 };
