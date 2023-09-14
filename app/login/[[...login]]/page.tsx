01db366efd52681994e172aceab9afcd6b951dbf
milliorn <scottmilliorn@gmai.com>
2023-09-13 22:04:24 -0700

    Registration

--- app/login/[[...login]]/page.tsx
+++ app/login/[[...login]]/page.tsx
@@ -1,7 +1,10 @@
+// Import the SignIn component from Clerk/Next.js
 import { SignIn } from "@clerk/nextjs";
 
+// Define the type for the component's props
 type Props = {};
 
+// Create the Registration component
 const Registration = (props: Props) => {
   return <SignIn />;
 };
